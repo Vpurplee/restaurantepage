7dfc30b5a1ad38ab2e41bca855772e3d72051458
Vitória Ferreira <123596433+Vpurplee@users.noreply.github.com>
2023-10-01 16:09:15 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,47 @@
+//  Código para criar o mapa, adicionar marcadores, etc.
+ function initMap() {
+ var map = new google.maps.Map(document.getElementById('map'), {
+ center: { lat: -34.397, lng: 150.644 },
+ zoom: 8 
+});
+ }
+// scroll
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  const target = document.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+  
+  function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    })
+  }
+  
+  animeScroll();
+  
+  if(target.length) {
+    window.addEventListener('scroll', debounce(function() {
+      animeScroll();
+    }, 200));
+  }
+// scroll
+
+
